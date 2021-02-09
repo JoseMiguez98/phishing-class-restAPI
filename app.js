223a9fb792e627e5a62209aee6ee3ccf6a9f5471
JoseMiguez98 <josemiguez184@gmail.com>
2021-02-09 17:25:46 -0300

    added cors support

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const app = require('express')();
 const db = require('./db/db');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// cors support
+app.use(cors());
 
 require('./config/config');
 
